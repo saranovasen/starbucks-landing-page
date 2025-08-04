8219dfe75b3f7c12a5e3ecb1f76b1ac197e8d42d
sarasenme <sarakairat7@gmail.com>
2025-08-04 19:04:56 +0300

    refactor: add tag to WeHaveSection  and scroll feature to Swiper

--- src/components/Swiper/Swiper.tsx
+++ src/components/Swiper/Swiper.tsx
@@ -1,22 +1,66 @@
+'use client';
+
+import clsx from 'clsx';
+import Image from 'next/image';
+import { useEffect, useRef } from 'react';
+
 import { swiperData } from '@/data/swiper';
 
 import { SwiperCard } from './SwiperCard';
 
 export const Swiper = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft = containerRef.current.scrollWidth;
+    }
+  }, []);
+
   return (
-    <div className="scrollbar-hide overflow-x-auto px-[5vw] xl:px-[7vw] 2xl:px-[10vw]">
-      <div className="grid auto-cols-max grid-flow-col gap-6 pb-4">
-        {swiperData.map((item) => (
-          <SwiperCard
-            key={item.id}
-            img={item.img}
-            title={item.title}
-            description={item.description}
-            price={item.price}
-            size={item.size}
-          />
-        ))}
-        <div className="w-[5vw] xl:w-[7vw] 2xl:w-[10vw]"></div>
+    <div className="relative">
+      <div
+        ref={containerRef}
+        className="scrollbar-hide overflow-x-auto px-[5vw] xl:px-[7vw] 2xl:px-[10vw]"
+      >
+        <div className="grid auto-cols-max grid-flow-col gap-6 pb-4">
+          {swiperData.map((item) => (
+            <SwiperCard
+              key={item.id}
+              img={item.img}
+              title={item.title}
+              description={item.description}
+              price={item.price}
+              size={item.size}
+            />
+          ))}
+          <div className="flex items-center justify-center">
+            <button
+              className={clsx(
+                'flex h-14 w-14 items-center justify-center',
+                'lg:h-21 lg:w-21',
+                'cursor-pointer transition-all duration-300 hover:scale-110',
+                'ml-4'
+              )}
+              onClick={() => {
+                if (containerRef.current) {
+                  containerRef.current.scrollBy({
+                    left: -300,
+                    behavior: 'smooth',
+                  });
+                }
+              }}
+            >
+              <Image
+                src="/assets/svg/ui-elements/arrow-boxed.svg"
+                alt="Previous"
+                width={84}
+                height={84}
+              />
+            </button>
+          </div>
+          <div className="w-[5vw] xl:w-[7vw] 2xl:w-[10vw]"></div>
+        </div>
       </div>
     </div>
   );
--- src/components/WeHaveSection/WeHaveSection.tsx
+++ src/components/WeHaveSection/WeHaveSection.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import Image from 'next/image';
 
 import { WeHaveSectionData } from '../../data/weHaveSection';
 import { Container } from '../Container';
@@ -10,7 +11,7 @@ export const WeHaveSection = () => {
   return (
     <section className="mb-16 md:mb-27">
       <Container>
-        <div className="bg-ui-light mx-auto w-[60%] max-w-screen-lg rounded-[1.75rem] px-10.5 py-5 md:w-[100%] lg:w-[80%] xl:px-25 xl:py-10">
+        <div className="bg-ui-light relative mx-auto w-[60%] max-w-screen-lg rounded-[1.75rem] px-10.5 py-5 md:w-[100%] lg:w-[80%] xl:px-25 xl:py-10">
           <div
             className={clsx(
               'flex flex-col gap-6 md:flex-row md:justify-between'
@@ -32,6 +33,32 @@ export const WeHaveSection = () => {
                 <DescText variant="secondary">{item.description}</DescText>
               </div>
             ))}
+          </div>
+          <div
+            className={clsx(
+              'absolute z-20',
+              '-top-10 -right-12 -translate-x-3',
+              'lg:-top-15 lg:-left-10 lg:-translate-x-8',
+              'drop-shadow-[-8px_8px_10px_rgba(0,0,0,0.50)]'
+            )}
+          >
+            <Image
+              width={187}
+              height={187}
+              src="assets/svg/ui-elements/tag.svg"
+              alt="Tag"
+              className={clsx('w-22 lg:w-38')}
+            />
+            <span
+              className={clsx(
+                'absolute inset-0 flex items-center justify-center text-center',
+                'text-lg uppercase lg:text-3xl',
+                'font-medium text-[var(--color-main-text)]'
+              )}
+            >
+              We <br />
+              have
+            </span>
           </div>
         </div>
       </Container>
